33bfa66d117d355ca59d8c72a1113873d42fc065
David Torro <davidtorro.business@gmail.com>
2025-11-02 19:20:36 +0100

    Create Methods to work with Threads

--- src/Models/Bus.java
+++ src/Models/Bus.java
@@ -1,6 +1,15 @@
 package Models;
 
 public class Bus {
+    /**
+     * constantes
+     * nunca he usado constantes en Java, pero creo que así se hace,
+     * considero que para este caso es mejor usar constantes que
+     * definir los valores directamente en los métodos
+     */
+    private static final int MIN_SPEED = 50;
+    private static final int MAX_SPEED = 80;
+
     // atributos
     private String plate;
     private int speed = 50;
@@ -31,5 +40,38 @@ public class Bus {
 
     public void setSpeed(int speed) {
         this.speed = speed;
+    }
+
+    // metodos
+    public synchronized void accelerate(int randomSpeed) {
+        // si la velocidad actual más la nueva supera el maximo, espera a ser notificado
+        while(this.speed + randomSpeed > MAX_SPEED) {
+            try {
+                wait(); // espera a ser notificado por otro hilo
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt(); // interrumpe el hilo en caso de error y no queda en loop infinito
+                System.out.println("ERROR: " + ex.getMessage());
+            }
+        }
+        // si no, acelera y lo dice (notify)
+        this.speed += randomSpeed;
+        System.out.println("Acelero " + randomSpeed + "km/h. Ahora vamos a: " + this.speed + "km/h.");
+        notifyAll(); // notifica a los hilos que estan esperando (brake)
+    }
+
+    public synchronized void brake(int randomSpeed) {
+        // si la velocidad actual menos la nueva es menor al minimo, espera a ser notificado
+        while(this.speed - randomSpeed < MIN_SPEED) {
+            try {
+                wait(); // espera a ser notificado por otro hilo
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt(); // interrumpe el hilo en caso de error y no queda en loop infinito
+                System.out.println("ERROR: " + ex.getMessage());
+            }
+        }
+        // si no, frena y lo dice
+        this.speed -= randomSpeed;
+        System.out.println("Freno " + randomSpeed + "km/h. Ahora vamos a: " + this.speed + "km/h.");
+        notifyAll(); // notifica a los hilos que estan esperando (accelerate)
     }
 }
--- src/Models/Speed.java
+++ src/Models/Speed.java
@@ -44,4 +44,26 @@ public class Speed extends Thread { // extiende de Thread para poder correr en h
     public void setAccelerating(boolean accelerating) {
         this.accelerating = accelerating;
     }
+
+    // metodos de la clase
+    // metodo run (de Thread) que se ejecuta al iniciar el hilo (@override)
+    @Override
+    public void run() {
+        do {
+            // generador de velocidad aleatoria entre 1 y 10
+            int randomSpeed = (int) (Math.random() * 10) + 1;
+
+            try {
+                if (accelerating) {
+                    bus.accelerate(randomSpeed);
+                } else {
+                    bus.brake(randomSpeed);
+                }
+                Thread.sleep(1000); // espera 1 segundo entre cada cambio de velocidad
+            } catch (Exception ex) {
+                Thread.currentThread().interrupt(); // interrumpe el hilo en caso de error y no queda en loop infinito
+                System.out.println("ERROR: " + ex.getMessage());
+            }
+        } while (true);
+    }
 }
